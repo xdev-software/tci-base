57a0bd274b33683063b71a6050059e5a56fedd0d
AB <a.bierler@xdev-software.de>
2024-05-03 11:40:45 +0200

    Create demo IT for webapp

---
+++ tci-advanced-demo/webapp-it/src/test/java/software/xdev/tci/demo/webapp/base/IntegrationTestDefaults.java
@@ -0,0 +1,92 @@
+package software.xdev.tci.demo.webapp.base;
+
+import java.time.Duration;
+import java.util.function.Function;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public interface IntegrationTestDefaults<SELF extends BaseTest>
+{
+	@SuppressWarnings("unchecked")
+	default SELF self()
+	{
+		return (SELF)this;
+	}
+	
+	default void navigateToOIDCLoginPage()
+	{
+		this.navigateTo("login");
+		
+		this.waitUntil(d -> d.findElement(By.xpath("//a[@href='/oauth2/authorization/local']")))
+			.click();
+		this.waitUntil(ExpectedConditions.urlContains(this.self().oidcInfra().getInternalHttpBaseEndPoint()));
+	}
+	
+	default void loginAndGotoMainSite()
+	{
+		this.loginAndGotoMainSite(
+			this.self().oidcInfra().getDefaultUserEmail(),
+			this.self().oidcInfra().getDefaultUserPassword());
+	}
+	
+	default void loginAndGotoMainSite(final String email, final String pw)
+	{
+		this.navigateToOIDCLoginPage();
+		
+		// Login on Identity server
+		this.doLoginOnOIDC(email, pw);
+		
+		this.checkForMainPage();
+	}
+	
+	default void navigateToLogout()
+	{
+		this.navigateTo("logout");
+	}
+	
+	default void logout()
+	{
+		this.navigateToLogout();
+		
+		this.waitUntil(d -> d.findElements(By.className("form-signin-heading")));
+		this.waitUntil(d -> d.findElement(By.xpath("//button[@type='submit']"))).click();
+	}
+	
+	default void navigateTo(final String... additionalPathSegments)
+	{
+		this.self().getWebDriver().get(this.self().getWebAppBaseUrl()
+			+ (additionalPathSegments.length > 0 ? ("/" + String.join("/", additionalPathSegments)) : ""));
+	}
+	
+	default void navigateToMainPage()
+	{
+		this.navigateTo();
+		this.checkForMainPage();
+	}
+	
+	default void checkForMainPage()
+	{
+		this.waitUntil(d -> d.findElements(By.className("container")));
+	}
+	
+	default void doLoginOnOIDC(final String email, final String pw)
+	{
+		this.waitUntil(d -> d.findElement(By.id("Input_Username"))).sendKeys(email);
+		this.waitUntil(d -> d.findElement(By.id("Input_Password"))).sendKeys(pw);
+		this.waitUntil(d -> d.findElement(By.xpath("//button[@value='login']"))).click();
+	}
+	
+	default <V> V waitUntil(final Function<WebDriver, V> isTrue)
+	{
+		return this.waitUntil(isTrue, Duration.ofSeconds(10));
+	}
+	
+	default <V> V waitUntil(final Function<WebDriver, V> isTrue, final Duration duration)
+	{
+		return new WebDriverWait(this.self().getWebDriver(), duration).until(isTrue);
+	}
+}
---
+++ tci-advanced-demo/webapp-it/src/test/java/software/xdev/tci/demo/webapp/cases/ProductTest.java
@@ -0,0 +1,58 @@
+package software.xdev.tci.demo.webapp.cases;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+
+import software.xdev.tci.demo.persistence.jpa.dao.ProductDAO;
+import software.xdev.tci.demo.tci.selenium.TestBrowser;
+import software.xdev.tci.demo.webapp.base.InfraPerCaseTest;
+import software.xdev.tci.demo.webapp.datageneration.ProductDG;
+
+
+class ProductTest extends InfraPerCaseTest
+{
+	@DisplayName("Create product")
+	@ParameterizedTest
+	@EnumSource(TestBrowser.class)
+	void create(final TestBrowser browser)
+	{
+		this.startAll(
+			browser,
+			dbtci -> dbtci.useNewEntityManager(em -> new ProductDG(em).generateProduct("PLACEHOLDER")));
+		
+		this.loginAndGotoMainSite();
+		
+		this.navigateTo("swagger-ui/index.html#/product-controller/create");
+		
+		this.waitUntil(d -> d.findElement(By.className("try-out__btn"))).click();
+		final WebElement taInput = this.waitUntil(d -> d.findElement(By.className("body-param__text")));
+		taInput.sendKeys(Keys.CONTROL + "a");
+		taInput.sendKeys(Keys.DELETE);
+		taInput.sendKeys("""
+			{
+			  "name": "TEST-ABC"
+			}""");
+		
+		this.waitUntil(d -> d.findElement(By.className("execute"))).click();
+		
+		final WebElement liveResponseTable = this.waitUntil(d -> d.findElement(By.className("live-responses-table")));
+		// First element contains response body
+		final String responseText = liveResponseTable.findElements(By.className("microlight")).get(0).getText();
+		// Id should be 2 as another product was created before
+		Assertions.assertEquals("""
+			{
+			  "id": 2,
+			  "name": "TEST-ABC"
+			}""", responseText);
+		
+		this.dbInfra().useNewEntityManager(em -> {
+			final ProductDAO productDAO = new ProductDAO(em);
+			Assertions.assertNotNull(productDAO.findByName("TEST-ABC"));
+		});
+	}
+}

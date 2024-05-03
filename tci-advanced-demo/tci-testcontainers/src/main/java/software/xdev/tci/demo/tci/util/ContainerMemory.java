e8b4a235fb1fb6bdd158d388e9eecfb8946d3d8b
AB <a.bierler@xdev-software.de>
2024-05-03 11:39:30 +0200

    Update ContainerMemory.java

--- tci-advanced-demo/tci-testcontainers/src/main/java/software/xdev/tci/demo/tci/util/ContainerMemory.java
+++ tci-advanced-demo/tci-testcontainers/src/main/java/software/xdev/tci/demo/tci/util/ContainerMemory.java
@@ -7,6 +7,7 @@ public final class ContainerMemory
 	public static final long M256M = M128M * 2;
 	public static final long M512M = M256M * 2;
 	public static final long M1G = M512M * 2;
+	public static final long M2G = M1G * 2;
 	
 	private ContainerMemory()
 	{

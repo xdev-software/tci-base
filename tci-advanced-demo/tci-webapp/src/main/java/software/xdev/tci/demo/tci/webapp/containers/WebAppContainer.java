fc9ff68dc96d99aaf34301445501b0c83d64e581
AB <a.bierler@xdev-software.de>
2024-05-03 11:39:48 +0200

    Create webapp TCI

---
+++ tci-advanced-demo/tci-webapp/src/main/java/software/xdev/tci/demo/tci/webapp/containers/WebAppContainer.java
@@ -0,0 +1,132 @@
+package software.xdev.tci.demo.tci.webapp.containers;
+
+import java.time.Duration;
+
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.HttpWaitStrategy;
+import org.testcontainers.containers.wait.strategy.WaitAllStrategy;
+
+import com.github.dockerjava.zerodep.shaded.org.apache.hc.core5.http.HttpStatus;
+
+
+public class WebAppContainer extends GenericContainer<WebAppContainer>
+{
+	public static final int DEFAULT_HTTP_PORT = 8080;
+	
+	protected final boolean connectionlessStart;
+	
+	public WebAppContainer(final String dockerImageName, final boolean connectionlessStart)
+	{
+		super(dockerImageName);
+		this.connectionlessStart = connectionlessStart;
+		if(connectionlessStart)
+		{
+			this.withConnectionlessStart();
+		}
+		this.addExposedPort(DEFAULT_HTTP_PORT);
+	}
+	
+	public WebAppContainer withDB(final String jdbcUrl, final String username, final String password)
+	{
+		return this.withEnv("SPRING_DATASOURCE_URL", jdbcUrl)
+			.withEnv("SPRING_DATASOURCE_USERNAME", username)
+			.withEnv("SPRING_DATASOURCE_PASSWORD", password);
+	}
+	
+	public WebAppContainer withAuth(
+		final String oidcClientId,
+		final String oidcClientSecret,
+		final String oidcServerUrl)
+	{
+		final String registration = "SPRING_SECURITY_OAUTH2_CLIENT_REGISTRATION_LOCAL_";
+		final String provider = "SPRING_SECURITY_OAUTH2_CLIENT_PROVIDER_LOCAL_";
+		if(this.connectionlessStart)
+		{
+			this.addEnv(registration + "AUTHORIZATION-GRANT-TYPE", "authorization_code");
+			this.addEnv(registration + "REDIRECT-URI", "{baseUrl}/{action}/oauth2/code/{registrationId}");
+			this.addEnv(provider + "AUTHORIZATION-URI", oidcServerUrl + "/connect/authorize");
+			this.addEnv(provider + "TOKEN-URI", oidcServerUrl + "/connect/token");
+			this.addEnv(provider + "JWK-SET-URI", oidcServerUrl + "/.well-known/openid-configuration/jwks");
+			this.addEnv(provider + "USER-INFO-URI", oidcServerUrl + "/connect/userinfo");
+			this.addEnv(provider + "USER-INFO-AUTHENTICATION-METHOD", "header");
+			this.addEnv(provider + "USER-NAME-ATTRIBUTE", "sub");
+		}
+		else
+		{
+			this.addEnv(provider + "ISSUER-URI", oidcServerUrl);
+		}
+		
+		return this.withEnv(registration + "CLIENT-NAME", "Local")
+			.withEnv(registration + "CLIENT-ID", oidcClientId)
+			.withEnv(registration + "CLIENT-SECRET", oidcClientSecret)
+			// offline_access is required to get the refresh token in the initial response!
+			.withEnv(registration + "SCOPE", "openid,profile,email,offline_access");
+	}
+	
+	public WebAppContainer withActuator(final String username, final String hash)
+	{
+		return this.withEnv("DEMO_ACTUATOR_USERNAME", username)
+			.withEnv("DEMO_ACTUATOR_PASSWORD-HASH", hash);
+	}
+	
+	public WebAppContainer withDisableHTTPS()
+	{
+		return this.withEnv("SERVER_SERVLET_SESSION_COOKIE_SECURE", false);
+	}
+	
+	/**
+	 * Starts the application without establishing any connections.<br/> Connection/Initialization is done on demand.
+	 */
+	public WebAppContainer withConnectionlessStart()
+	{
+		final String springJpa = "SPRING_JPA_";
+		return this.withDisableFlyway()
+			// Disable DB for actuator endpoint or complete endpoint will fail otherwise
+			.withEnv("MANAGEMENT_HEALTH_DB_ENABLED", false)
+			.withEnv(
+				springJpa + "DATABASE-PLATFORM",
+				"software.xdev.tci.demo.persistence.config.dialect.DefaultDialect")
+			.withEnv(springJpa + "PROPERTIES_HIBERNATE_TEMP_USE_JDBC_METADATA_DEFAULTS", false);
+	}
+	
+	public WebAppContainer withDisableFlyway()
+	{
+		return this.withEnv("SPRING_FLYWAY_ENABLED", false);
+	}
+	
+	public WebAppContainer withEnv(final String key, final boolean value)
+	{
+		return this.withEnv(key, String.valueOf(value));
+	}
+	
+	public WebAppContainer withDefaultWaitStrategy(
+		final Duration startUpTimeout,
+		final String actuatorUsername,
+		final String actuatorPassword)
+	{
+		return this.waitingFor(new WaitAllStrategy()
+			.withStartupTimeout(startUpTimeout)
+			.withStrategy(
+				new HttpWaitStrategy()
+					.forPort(WebAppContainer.DEFAULT_HTTP_PORT)
+					.forPath("/robots.txt")
+					.forStatusCode(HttpStatus.SC_OK)
+					.withReadTimeout(Duration.ofSeconds(10))
+			)
+			.withStrategy(
+				new HttpWaitStrategy()
+					.forPort(WebAppContainer.DEFAULT_HTTP_PORT)
+					.forPath("/actuator/health")
+					.withBasicCredentials(actuatorUsername, actuatorPassword)
+					.forStatusCode(HttpStatus.SC_OK)
+					.withReadTimeout(Duration.ofSeconds(10))
+			));
+	}
+	
+	@SuppressWarnings("unused")
+	private void remoteDebug(final int port)
+	{
+		this.addEnv("JAVA_OPTS", "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:" + port);
+		this.addFixedExposedPort(port, port);
+	}
+}
---
+++ tci-advanced-demo/tci-webapp/src/main/java/software/xdev/tci/demo/tci/webapp/containers/WebAppContainerBuilder.java
@@ -0,0 +1,72 @@
+package software.xdev.tci.demo.tci.webapp.containers;
+
+import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import software.xdev.testcontainers.imagebuilder.AdvancedImageFromDockerFile;
+
+
+public final class WebAppContainerBuilder
+{
+	private static final Logger LOG = LoggerFactory.getLogger(WebAppContainerBuilder.class);
+	private static final Logger LOG_CONTAINER_BUILD =
+		LoggerFactory.getLogger("container.build.webapp");
+	
+	private static String builtImageName;
+	
+	private WebAppContainerBuilder()
+	{
+	}
+	
+	public static synchronized String getBuiltImageName()
+	{
+		if(builtImageName != null)
+		{
+			return builtImageName;
+		}
+		
+		LOG.info("Building WebApp-DockerImage...");
+		
+		final AdvancedImageFromDockerFile builder =
+			new AdvancedImageFromDockerFile("webapp-it-local", false)
+				.withLoggerForBuild(LOG_CONTAINER_BUILD)
+				.withAdditionalIgnoreLines(
+					// Ignore git-folder, as it will be provided in the Dockerfile
+					".git/**",
+					// Ignore other unused folders and extensions
+					".iml",
+					".md",
+					"target/**",
+					".config/**",
+					".idea/**",
+					"_dev_infra/**",
+					// Ignore not required test-modules that may have changed
+					// sources only - otherwise the parent pom doesn't find the resources
+					"tci-*/src/**",
+					"persistence-it/src/**",
+					"webapp-it/src/**",
+					// Ignore resources that are just used for development
+					"webapp/src/main/resources-dev/**")
+				.withDockerFilePath(Paths.get("../tci-webapp/Dockerfile"))
+				.withBaseDir(Paths.get("../"))
+				// File is in root directory - we can't access it
+				.withBaseDirRelativeIgnoreFile(null);
+		
+		try
+		{
+			builtImageName = builder.get(5, TimeUnit.MINUTES);
+		}
+		catch(final TimeoutException tex)
+		{
+			throw new IllegalStateException("Timed out", tex);
+		}
+		
+		LOG.info("Built Image; Name ='{}'", builtImageName);
+		
+		return builtImageName;
+	}
+}

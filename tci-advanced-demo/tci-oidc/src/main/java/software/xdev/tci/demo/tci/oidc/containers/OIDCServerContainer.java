f66080a67ff13bc88af202da66741ceda918826b
AB <a.bierler@xdev-software.de>
2024-05-03 11:39:03 +0200

    Create OIDC example

---
+++ tci-advanced-demo/tci-oidc/src/main/java/software/xdev/tci/demo/tci/oidc/containers/OIDCServerContainer.java
@@ -0,0 +1,90 @@
+package software.xdev.tci.demo.tci.oidc.containers;
+
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.utility.DockerImageName;
+
+
+public class OIDCServerContainer extends GenericContainer<OIDCServerContainer>
+{
+	public static final int PORT = 8080;
+	
+	public static final String DEFAULT_CLIENT_ID = "client-id1";
+	public static final String DEFAULT_CLIENT_SECRET = "client-secret1";
+	
+	public OIDCServerContainer()
+	{
+		super(DockerImageName.parse("ghcr.io/soluto/oidc-server-mock:0.9.0"));
+		this.addExposedPort(PORT);
+	}
+	
+	public OIDCServerContainer withDefaultEnvConfig()
+	{
+		this.addEnv("ASPNETCORE_ENVIRONMENT", "Development");
+		this.addEnv("ASPNET_SERVICES_OPTIONS_INLINE", """
+			{
+			  "ForwardedHeadersOptions": {
+			    "ForwardedHeaders" : "All"
+			  }
+			}
+			""");
+		this.addEnv("SERVER_OPTIONS_INLINE", """
+			{
+			  "AccessTokenJwtType": "JWT",
+			  "Discovery": {
+			    "ShowKeySet": true
+			  },
+			  "Authentication": {
+			    "CookieSameSiteMode": "Lax",
+			    "CheckSessionCookieSameSiteMode": "Lax"
+			  }
+			}
+			""");
+		this.addEnv("LOGIN_OPTIONS_INLINE", """
+			{
+			  "AllowRememberLogin": false
+			}
+			""");
+		this.addEnv("LOGOUT_OPTIONS_INLINE", """
+			{
+			  "AutomaticRedirectAfterSignOut": true
+			}
+			""");
+		this.addEnv("CLIENTS_CONFIGURATION_INLINE", """
+			[
+			  {
+			      "ClientId": "%s",
+			      "ClientSecrets": [
+			          "%s"
+			      ],
+			      "Description": "TimelineDesc",
+			      "AllowedGrantTypes": [
+			          "authorization_code",
+			          "refresh_token"
+			      ],
+			      "RedirectUris": [
+			          "*"
+			      ],
+			      "AllowedScopes": [
+			          "openid",
+			          "profile",
+			          "email",
+			          "offline_access"
+			      ],
+			      "AlwaysIncludeUserClaimsInIdToken": true,
+			      "AllowOfflineAccess": true,
+			      "RequirePkce": false
+			  }
+			]
+			""".formatted(DEFAULT_CLIENT_ID, DEFAULT_CLIENT_SECRET));
+		return this.self();
+	}
+	
+	public String getExternalHttpBaseEndPoint()
+	{
+		// noinspection HttpUrlsUsage
+		return "http://"
+			+ this.getHost()
+			+ ":"
+			+ this.getMappedPort(PORT);
+	}
+}

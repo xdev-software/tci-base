6d3f0e73a592a7151ffef8e931fe789d6c378572
AB <a.bierler@xdev-software.de>
2025-06-12 17:34:42 +0200

    Implement more efficient wait strategy

--- tci-advanced-demo/tci-db/src/main/java/software/xdev/tci/demo/tci/db/containers/DBContainer.java
+++ tci-advanced-demo/tci-db/src/main/java/software/xdev/tci/demo/tci/db/containers/DBContainer.java
@@ -1,15 +1,18 @@
 package software.xdev.tci.demo.tci.db.containers;
 
+import org.testcontainers.containers.wait.strategy.WaitStrategy;
 import org.testcontainers.utility.DockerImageName;
 
 
-public class DBContainer extends MariaDBContainer<DBContainer>
+public class DBContainer
+	extends MariaDBContainer<DBContainer>
+	implements WaitableJDBCContainer
 {
 	public static final int PORT = MARIADB_PORT;
 	
 	public DBContainer(final String image)
 	{
-		super(DockerImageName.parse("mariadb:11"));
+		super(DockerImageName.parse(image));
 		
 		// (31.03.2022 AB)NOTE: https://github.com/testcontainers/testcontainers-java/issues/914
 		// Do not mount the volume!
@@ -17,5 +20,19 @@ public class DBContainer extends MariaDBContainer<DBContainer>
 		// DO NOT resolve client hostnames for more performance
 		// https://mariadb.com/docs/server/ref/mdb/system-variables/skip_name_resolve/
 		this.setCommand("--skip-name-resolve");
+		
+		this.waitingFor(this.completeJDBCWaitStrategy());
+	}
+	
+	@Override
+	public WaitStrategy getWaitStrategy()
+	{
+		return super.getWaitStrategy();
+	}
+	
+	@Override
+	public void waitUntilContainerStarted()
+	{
+		WaitableJDBCContainer.super.waitUntilContainerStarted();
 	}
 }
